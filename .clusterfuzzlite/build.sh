8a27e77bd5afd65b663f1a58f821b3fb16802066
Ben Civjan <bcivjan2@illinois.edu>
2022-04-01 18:26:38 +0200

    Testing workflow

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -4,16 +4,16 @@
 
 # Build fuzzers into $OUT. These could be detected in other ways.
 # for fuzzer in $(find $SRC -name '*_fuzzer.py'); do
-
-# fuzzer_basename=$(basename -s .py $fuzzer)
-# fuzzer_package=${fuzzer_basename}.pkg
-find . -name '*_fuzzer' -exec cp -v '{}' $OUT ';'
+fuzzer='target_fuzz.py'
+fuzzer_basename=$(basename -s .py $fuzzer)
+fuzzer_package=${fuzzer_basename}.pkg
+# find . -name '*_fuzzer' -exec cp -v '{}' $OUT ';'
 
   # To avoid issues with Python version conflicts, or changes in environment
   # over time, we use pyinstaller to create a standalone
   # package. Though not necessarily required for reproducing issues, this is
   # required to keep fuzzers working properly.
-  pyinstaller --distpath $OUT --onefile --name $fuzzer_package $fuzzer
+pyinstaller --distpath $OUT --onefile --name $fuzzer_package $fuzzer
 
   # Create execution wrapper. Atheris requires that certain libraries are
   # preloaded, so this is also done here to ensure compatibility and simplify
@@ -22,11 +22,11 @@ find . -name '*_fuzzer' -exec cp -v '{}' $OUT ';'
   # NOTE: If you are fuzzing python-only code and do not have native C/C++
   # extensions, then remove the LD_PRELOAD line below as preloading sanitizer
   # library is not required and can lead to unexpected startup crashes.
-  echo "#!/bin/sh
+echo "#!/bin/sh
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
 LD_PRELOAD=\$this_dir/sanitizer_with_fuzzer.so \
 ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
 \$this_dir/$fuzzer_package \$@" > $OUT/$fuzzer_basename
-  chmod +x $OUT/$fuzzer_basename
+chmod +x $OUT/$fuzzer_basename
 done
