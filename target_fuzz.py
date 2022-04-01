eb121a5d49ed132a3e38687d3eb6e9b3dab24f7a
Ben Civjan <bcivjan2@illinois.edu>
2022-04-01 17:52:17 +0200

    Testing workflow

---
+++ target_fuzz.py
@@ -0,0 +1,16 @@
+import atheris
+
+with atheris.instrument_imports():
+  from html.parser import HTMLParser
+  import sys
+
+def TestOneInput(data):
+    try:
+        string = data.decode('utf-8')
+        parser = HTMLParser()
+        parser.feed(string)
+    except UnicodeDecodeError:
+        pass
+
+atheris.Setup(sys.argv, TestOneInput)
+atheris.Fuzz()
